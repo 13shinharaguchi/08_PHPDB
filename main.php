94ce13ac9e2391004ae06a7cbabd5fc7c149503c
13shinharaguchi <shin13usa@icloud.com>
2023-01-19 23:25:59 +0900

    update

--- main.php
+++ main.php
@@ -34,7 +34,7 @@ $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="css/main.css">
     <title>メイン</title>
 </head>
 
